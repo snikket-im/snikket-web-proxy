c3018b06cf43bfd4fe157a976d4eb5a0464cbde9
Matthew Wild <mwild1@gmail.com>
2020-12-11 16:47:31 +0000

    Honour SNIKKET_TWEAK_HTTP/S_PORT variables

--- cert-monitor.sh
+++ cert-monitor.sh
@@ -2,11 +2,16 @@
 
 CERT_PATH="/snikket/letsencrypt/live/$SNIKKET_DOMAIN/cert.pem"
 
+export SNIKKET_TWEAK_HTTP_PORT=${SNIKKET_TWEAK_HTTP_PORT-80}
+export SNIKKET_TWEAK_HTTPS_PORT=${SNIKKET_TWEAK_HTTP_PORT-443}
+export SNIKKET_TWEAK_INTERNAL_HTTP_PORT=${SNIKKET_TWEAK_INTERNAL_HTTP_PORT-5280}
+
 while sleep 10; do
 	if test -f "$CERT_PATH"; then
 		for proto in http https; do
-			sed "s/SNIKKET_DOMAIN/$SNIKKET_DOMAIN/g" /etc/nginx/templates/$proto \
-			    > /etc/nginx/sites-enabled/$proto;
+			envsubst '$SNIKKET_DOMAIN $SNIKKET_TWEAK_HTTP_PORT $SNIKKET_TWEAK_HTTPS_PORT $SNIKKET_TWEAK_INTERNAL_HTTP_PORT' \
+			  < /etc/nginx/templates/$proto \
+			  > /etc/nginx/sites-enabled/$proto;
 		done
 		/usr/sbin/nginx -s reload
 		exit 0;
