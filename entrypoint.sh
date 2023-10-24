37861987b4e4142a39bd068bb7687a9d57fd4509
Matthew Wild <mwild1@gmail.com>
2023-10-24 14:16:48 +0100

    Allow customizing which protocols get enabled (#5)
    This allows customization of which "protocol" templates get rendered. Alone,
    it's not very useful. However, a subsequent image layer could add additional
    templates and enable a custom set using this variable.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,12 @@
 
 CERT_PATH="/snikket/letsencrypt/live/$SNIKKET_DOMAIN/fullchain.pem"
 
+PROTOS="${SNIKKET_TWEAK_WEB_PROXY_PROTOS:-http https}"
+
 if test -f "$CERT_PATH"; then
 	## Certs already exist - render and deploy configs
 	/usr/local/bin/render-template.sh "/etc/nginx/templates/snikket-common" "/etc/nginx/snippets/snikket-common.conf"
-	for proto in http https; do
+	for proto in $PROTOS; do
 		/usr/local/bin/render-template.sh "/etc/nginx/templates/$proto" "/etc/nginx/sites-enabled/$proto";
 	done
 else
